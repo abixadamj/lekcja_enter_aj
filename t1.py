70f942d85b19cc25614b3ab873c3d38f92fd3e4c
Adam Jurkiewicz <a.jurkiewicz@abix.info.pl>
2020-02-01 13:24:30 +0100

    Pierwsza wersja pliku

---
+++ t1.py
@@ -0,0 +1,22 @@
+import turtle
+z = turtle.Turtle()
+
+def kwadrat(pozycja, bok):
+    z.pu()
+    z.goto(pozycja)
+    z.pd()
+    for k in range(4):
+        z.fd(bok)
+        z.rt(90)
+
+
+b = 10
+p = 20, 60
+r = -20, -30
+#kwadrat(p, b)
+#kwadrat(r, b)
+for i in range(6):
+    print(f"i wynosi {i}")
+    kwadrat(p, b * i)
+
+
